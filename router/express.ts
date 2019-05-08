365e094ddc1123532cd11e27a8210ce4f86c23c6
Endel Dreyer <endel.dreyer@gmail.com>
2019-05-08 18:25:20 -0300

    allow to send jwt token in 'token' query string

--- router/express.ts
+++ router/express.ts
@@ -19,6 +19,14 @@ declare global {
 const jwtMiddleware = jwt({
     secret: JWT_SECRET,
     userProperty: "auth",
+    getToken: function (req) {
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+            return req.headers.authorization.split(' ')[1];
+        } else if (req.query && req.query.token) {
+            return req.query.token;
+        }
+        return null;
+    }
 });
 
 const route = express.Router();
