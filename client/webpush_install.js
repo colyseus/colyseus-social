841efff1980f28ed1a410982c21a826453765db6
Endel Dreyer <endel.dreyer@gmail.com>
2019-05-17 21:15:09 -0300

    add webpush service worker placeholder scripts

---
+++ client/webpush_install.js
@@ -0,0 +1,31 @@
+const check = () => {
+    if (!("serviceWorker" in navigator)) {
+      throw new Error("No Service Worker support!");
+    }
+    if (!("PushManager" in window)) {
+      throw new Error("No Push API Support!");
+    }
+  };
+
+  const registerServiceWorker = async () => {
+    const swRegistration = await navigator.serviceWorker.register("webpush_service.js");
+    return swRegistration;
+  };
+
+  const requestNotificationPermission = async () => {
+    const permission = await window.Notification.requestPermission();
+    // value of permission can be 'granted', 'default', 'denied'
+    // granted: user has accepted the request
+    // default: user has dismissed the notification permission popup by clicking on x
+    // denied: user has denied the request.
+    if (permission !== "granted") {
+      throw new Error("Permission not granted for Notification");
+    }
+  };
+
+  const main = async () => {
+    check();
+    const swRegistration = await registerServiceWorker();
+    const permission = await requestNotificationPermission();
+  };
+  // main(); we will not call main in the beginning.
---
+++ client/webpush_service.js
@@ -0,0 +1,90 @@
+const BACKEND_URL = "http://localhost:3000/push";
+const FRONTEND_URL = "http://localhost:8000/";
+
+// urlB64ToUint8Array is a magic function that will encode the base64 public key
+// to Array buffer which is needed by the subscription option
+const urlB64ToUint8Array = base64String => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
+
+const saveSubscription = async subscription => {
+  const response = await fetch(`${BACKEND_URL}/subscribe`, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(subscription)
+  });
+  return response.json();
+};
+
+const getApplicationServerKey = async () => {
+  const response = await fetch(`${BACKEND_URL}/web`, {
+    method: "get",
+    headers: { "Content-Type": "application/json" }
+  });
+  return response.json();
+}
+
+
+self.addEventListener("install", async () => {
+  // This will be called only once when the service worker is installed for first time.
+  try {
+    const serverKey = await getApplicationServerKey();
+    const applicationServerKey = urlB64ToUint8Array(serverKey.publicKey);
+    const options = { applicationServerKey, userVisibleOnly: true };
+    const subscription = await self.registration.pushManager.subscribe(options);
+    const response = await saveSubscription(subscription);
+  } catch (err) {
+    console.log("Error", err);
+  }
+});
+
+self.addEventListener("push", function(event) {
+  if (event.data) {
+    const data = event.data.json();
+
+    if (Notification.permission === "granted") {
+      // show notification!
+      self.registration.showNotification(data.title, data);
+    }
+
+    console.log("Push event!! ", event.data.text());
+
+  } else {
+    console.log("Push event but no data");
+  }
+});
+
+
+self.addEventListener('notificationclick', function (event) {
+  // Android doesn't close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(windowClients => {
+      // Check if there is already a window/tab open with the target URL
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+
+        // If so, just focus it.
+        if (client.url.indexOf(FRONTEND_URL) >= 0 && 'focus' in client) {
+          return client.focus();
+        }
+      }
+
+      // If not, then open the target URL in a new window/tab.
+      if (clients.openWindow) {
+        return clients.openWindow(FRONTEND_URL);
+      }
+    })
+  );
+});
